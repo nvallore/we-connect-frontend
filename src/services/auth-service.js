94aee6e4b6b7e98d478a2e0f0596197b74f6ed9e
Akshaya Shankar <akshaya_shankar@comcast.com>
2022-09-09 01:01:25 +0530

    Initial checkout

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -6,6 +6,7 @@ export function login(loginDetails) {
         username: loginDetails?.registrationId,
         password: loginDetails?.password
     };
+    // var response;
     return axios.post('http://127.0.0.1:8000/api/login', request).then(response => {
                 // response = {
                 //     isFirstTimeLogin: true,
