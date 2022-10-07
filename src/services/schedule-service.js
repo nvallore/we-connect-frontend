3f2b66617ff17d89aa70685052235fbd7cf21327
Nikhil Vallore <nvallore@salesforce.com>
2022-10-07 16:06:28 +0530

    schedule service var name changed

--- src/services/schedule-service.js
+++ src/services/schedule-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// const profileHost = 'http://127.0.0.1:3001/scheduler';
+// const scheduleHost = 'http://127.0.0.1:3001/scheduler';
 const scheduleHost = 'http://localhost/scheduler';
 
 
